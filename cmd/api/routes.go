5417fea4ff1dc3c7985cae26247560eb18e540a5
Duane M.  Arzu <2020152202@ub.edu.bz>
2024-11-08 22:27:47 -0600

    test2 testing 6

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,23 +15,23 @@ func (a *applicationDependencies) routes() http.Handler {
 	router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedResponse)
 
 	//Product part
-	router.HandlerFunc(http.MethodGet, "/healthcheck", a.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/product", a.listProductHandler)
-	router.HandlerFunc(http.MethodPost, "/product", a.createProductHandler)
-	router.HandlerFunc(http.MethodGet, "/product/:pid", a.displayProductHandler)
-	router.HandlerFunc(http.MethodPatch, "/product/:pid", a.updateProductHandler)
-	router.HandlerFunc(http.MethodDelete, "/product/:pid", a.deleteProductHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthcheckHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/product", a.listProductHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/product", a.createProductHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/product/:pid", a.displayProductHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/product/:pid", a.updateProductHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/product/:pid", a.deleteProductHandler)
 
 	// //Review part
-	router.HandlerFunc(http.MethodGet, "/review", a.listReviewHandler)
-	router.HandlerFunc(http.MethodPost, "/review", a.createReviewHandler)
-	router.HandlerFunc(http.MethodGet, "/review/:rid", a.displayReviewHandler)
-	router.HandlerFunc(http.MethodPatch, "/review/:rid", a.updateReviewHandler)
-	router.HandlerFunc(http.MethodDelete, "/review/:rid", a.deleteReviewHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/review", a.listReviewHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/review", a.createReviewHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/review/:rid", a.displayReviewHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/review/:rid", a.updateReviewHandler)
+	router.HandlerFunc(http.MethodDelete, "/v1/review/:rid", a.deleteReviewHandler)
 
-	router.HandlerFunc(http.MethodGet, "/product-review/:rid", a.listProductReviewHandler)
-	router.HandlerFunc(http.MethodGet, "/product/:pid/review/:rid", a.getProductReviewHandler)
-	router.HandlerFunc(http.MethodPatch, "/helpful-count/:rid", a.HelpfulCountHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/product-review/:rid", a.listProductReviewHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/product/:pid/review/:rid", a.getProductReviewHandler)
+	router.HandlerFunc(http.MethodPatch, "/v1/helpful-count/:rid", a.HelpfulCountHandler)
 
 	return a.recoverPanic(a.rateLimit(router))
 
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module github.com/Duane-Arzu/test2
 go 1.22.6
 
 require (
-	github.com/Duane-Arzu/test1 v0.0.0-20241102015409-59c73288db79
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.9
 )
